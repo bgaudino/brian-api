3c848f95edabde08e405d8fee3f029d8ea2cc3a8
brianpaperless <brian@thepaperlessagent.com>
2022-01-03 10:31:10 -0600

    Add music app

---
+++ music/helpers.py
@@ -0,0 +1,27 @@
+import requests
+from .models import Score
+from random import randint
+
+
+def create_fake_scores(num_scores):
+    try:
+        num_scores = int(num_scores)
+    except ValueError:
+        print("Please enter a valid number of scores to create.")
+        return
+
+    for _ in range(num_scores):
+        url = "https://random-data-api.com/api/users/random_user"
+        res = requests.get(url).json()
+        name = res["first_name"]
+        num_attempted = randint(1, 50)
+        game_type = ["note_id", "interval_ear_training"][randint(0, 1)]
+        num_correct = randint(0, num_attempted)
+        score = Score(
+            name=name,
+            num_attempted=num_attempted,
+            num_correct=num_correct,
+            game_type=game_type
+        )
+        score.save()
+    print(f"Created {num_scores} fake scores")
---
+++ music/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from user.models import User
+
+
+class Score(models.Model):
+    GAME_TYPES = (
+        ("note_id", "Note Identification"),
+        ("interval_ear_training", "Interval Ear Training"),
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    game_type = models.CharField(max_length=50, choices=GAME_TYPES)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='scores', null=True, blank=True)
+    num_correct = models.IntegerField(default=0)
+    num_attempted = models.IntegerField(default=0)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    def save(self, *args, **kwargs):
+        self.percentage = (
+            (self.num_correct / self.num_attempted) * 100
+        ) if self.num_attempted else 0
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}: ({self.num_correct}/{self.num_attempted}) {self.percentage}%"
