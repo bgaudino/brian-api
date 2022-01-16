5c373940895f9629657f9eb30ff9b2d3d07e5e38
brianpaperless <brian@thepaperlessagent.com>
2022-01-16 09:12:03 -0600

    Add store

--- shopping/serializers.py
+++ shopping/serializers.py
@@ -5,4 +5,4 @@ from .models import Item
 class ItemSerializer(ModelSerializer):
     class Meta:
         model = Item
-        fields = ('id', 'name', 'is_purchased', 'purchased_at')
+        fields = ('id', 'name', 'store', 'is_purchased', 'purchased_at')
